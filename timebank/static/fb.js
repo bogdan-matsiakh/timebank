ac1dc23f0883cccd4f8e920354910d51ddfe2b6b
bogdan-matsiakh <bmatsiakh@gmail.com>
2013-03-03 10:57:13 +0200

    get friends

--- timebank/static/fb.js
+++ timebank/static/fb.js
@@ -1,31 +1,3 @@
-window.fbAsyncInit = function() {
-    // init the FB JS SDK
-    FB.init({
-        appId : '109464542535160', // App ID from the App Dashboard
-        channelUrl : 'http://lit-woodland-6028.herokuapp.com/channel', // Channel File for x-domain communication
-        status : true, // check the login status upon init?
-        cookie : true, // set sessions cookies to allow your server to access the session?
-        xfbml : true // parse XFBML tags on this page?
-
-    });
-
-    FB.getLoginStatus(function(response) {
-        console.log(response)
-        if (response.status === 'connected') {
-            console.log('connected');
-            facebook.getMe();
-            facebook.getFriends();
-        } else if (response.status === 'not_authorized') {
-            console.log('not_authorized')
-        } else {
-            console.log('not_logged_in');
-        }
-    });
-
-    // Additional initialization code such as adding Event Listeners goes here
-
-};
-
 facebook = (function () {
     
     
@@ -55,7 +27,35 @@ facebook = (function () {
             });
         }
     }
-})()
+})();
+
+window.fbAsyncInit = function() {
+    // init the FB JS SDK
+    FB.init({
+        appId : '109464542535160', // App ID from the App Dashboard
+        channelUrl : 'http://lit-woodland-6028.herokuapp.com/channel', // Channel File for x-domain communication
+        status : true, // check the login status upon init?
+        cookie : true, // set sessions cookies to allow your server to access the session?
+        xfbml : true // parse XFBML tags on this page?
+
+    });
+
+    FB.getLoginStatus(function(response) {
+        console.log(response)
+        if (response.status === 'connected') {
+            console.log('connected');
+            facebook.getMe();
+            facebook.getFriends();
+        } else if (response.status === 'not_authorized') {
+            console.log('not_authorized')
+        } else {
+            console.log('not_logged_in');
+        }
+    });
+    // Additional initialization code such as adding Event Listeners goes here
+
+};
+
 
 (function(d, debug) {
         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
