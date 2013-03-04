bbf7e588f68a4fcae480a3e912b346082a968ac0
bogdan-matsiakh <bmatsiakh@gmail.com>
2013-03-05 00:38:49 +0200

    move js

--- timebank/static/js/app.js
+++ timebank/static/js/app.js
@@ -1,6 +1,16 @@
 require(
     ['app/fb', 'app/main'],
     function( tb ){
-        
+        FB.getLoginStatus(function(response) {
+                        if (response.status === 'connected') {
+                            console.log('connected');
+                            tb.facebook.getMe();
+                            tb.facebook.getFriends();
+                        } else if (response.status === 'not_authorized') {
+                            console.log('not_authorized')
+                        } else {
+                            console.log('not_logged_in');
+                        }
+                    });
     }
 );
