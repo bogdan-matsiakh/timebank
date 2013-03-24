799d9cc8c0787d8225a00294c447105f3a0fec90
bogdan-matsiakh <bmatsiakh@gmail.com>
2013-03-25 00:50:33 +0200

    no facebook

--- timebank/settings.py
+++ timebank/settings.py
@@ -16,15 +16,15 @@ MANAGERS = ADMINS
 # Parse database configuration from $DATABASE_URL
 # for heroku server
 
-'''
+
 DATABASES = {
     'default':  dj_database_url.config()
 }
-'''
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # for local run
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -36,6 +36,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+'''
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
